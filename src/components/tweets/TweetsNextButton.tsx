91eb8cb4cf790e08d0c5643dd36769e616414dfc
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-05-23 12:19:09 +0700

    add React import

--- src/components/tweets/TweetsNextButton.tsx
+++ src/components/tweets/TweetsNextButton.tsx
@@ -1,9 +1,7 @@
+import type { RefObject } from 'react';
 import { useEffect, useRef } from 'react';
 
-const useOnVisible = (
-  ref: React.RefObject<HTMLElement>,
-  callback: () => void
-) => {
+const useOnVisible = (ref: RefObject<HTMLElement>, callback: () => void) => {
   useEffect(() => {
     if (!ref.current) return;
 
@@ -43,9 +41,7 @@ export const TweetsNextButton = ({
 
   useOnVisible(ref, fetchNextPage);
 
-  const nextPageStatus = hasNextPage
-    ? 'Loading...'
-    : 'There is not more tweets';
+  const nextPageStatus = hasNextPage ? 'Loading...' : 'There is not more tweets';
 
   return (
     <button ref={ref} className="block py-4">
